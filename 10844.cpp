2a7c6d95b7e002264d2ca0e1c2abfcf81baf9329
tinwoon <joqom@naver.com>
2020-02-07 17:48:53 +0900

    Add files via upload

--- 10844.cpp
+++ 10844.cpp
@@ -1,33 +1,31 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-//점화식을 이용
-//dp문제는 대부분이 점화식이 있는데 이 부분을 유추하기가 굉장히 수학적인 부분
-//dp공부를 하는건지 점화식을 공부해야하는지 모르겠음.
+using namespace std;
 
-int main(void) {
-	int num;
-	int dp[101][10] = {};
-	int sum = 0;
-	scanf("%d", &num);
-	for (int i = 0; i < 10; i++)
-		dp[1][i] = 1;
-
-	for (int i = 2; i <= num; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (j == 0)
-				dp[i][0] = dp[i - 1][1] % 1000000000;
-			else if (j == 9)
-				dp[i][9] = dp[i - 1][8] % 1000000000;
-			else
-				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
-		}
-	}
-	for (int i = 1; i < 10; i++)
-		sum = (sum + dp[num][i]) % 1000000000;
-
-	printf("%d\n", sum% 1000000000);
+void calculate() {
+	vector<int> dp;
+	dp.resize(num);
+	dp[0] = value[0];
+	dp[1] = value[0] + value[1];
+	dp[2] = max(dp[1],dp[0]+value)
 }
 
 
+int main() {
+	int num = 0;
+	scanf("%d", &num);
+	vector <int> value;
+	for (int i = 0; i < num; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		value.push_back(tmp);
+	}
 
 
+
+	return 0;
+	
+
+}
