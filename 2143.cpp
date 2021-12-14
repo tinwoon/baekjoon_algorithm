556d210bc3378953122dc3d8490516b5e5ef339d
jiwon oh <tinwoon21@gmail.com>
2021-12-14 23:45:53 +0900

    2143 두 배열의 합 풀이 중

---
+++ 2143.cpp
@@ -0,0 +1,17 @@
+﻿#pragma warning (disable : 4996)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+//2143 두 배열의 합
+//이분 탐색과 부분합을 사용하는 문제로 보여진다.
+//풀이 방법은 투포인터 방식 및 부분합 방식으로 풀이 예정
+
+int T;
+
+
+
+int main() {
+
+}
