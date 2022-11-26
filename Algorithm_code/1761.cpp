d8002b0b66df9142d15b8f3fe8b1cbfd86853a25
jiwon oh <tinwoon21@gmail.com>
2022-11-26 13:12:34 +0900

    1761 정점들의 거리

--- Algorithm_code/1761.cpp
+++ Algorithm_code/1761.cpp
@@ -5,24 +5,22 @@
 #include <tuple>
 #include <map>
 
+//1761번 정점들의 거리 문제
+//a와 b 사이의 LCA(최소 공통 조상)를 구한 뒤 해당 조상부터 a까지의 거리 + 해당 조상으로부터 b까지의 거리를 구하는 방법으로 진행함.
+
 int N, M;
 //connected[a]에 연결된 노드들은 first이며, a와 b 사이의 거리는 second
 std::vector< std::map<int, int> > connected;
 std::vector<bool> visited;
 std::vector<int> depth;
 std::vector<int> parent;
-//BFS 기준으로 indexing 하기 위함(BFS에서 첫번째로 방문한 노드는 1번, 두번째는 4번 등등)
-std::vector<int> index;
-//root 노드에서 특정 노드까지의 부분합을 넣기 위함.(6번노드와 5번노드 간의 거리는 1 -> 6 과 1 -> 5의 거리를 빼면 된다.
-std::vector<int> dp;
+//root 노드로 부터 n노드까지의 간선의 값들의 합을 저장해두는 배열(구간 합처럼 사용할 수 있다)
+std::vector<int> sum;
 
 void bfs() {
-	int idx = 1;
 	visited[1] = true;
 	depth[1] = 0;
 	parent[1] = 0;
-	index[1] = idx;
-	dp[idx] = 0;
 
 	//node, depth
 	std::queue<std::pair<int, int> > q;
@@ -37,14 +35,13 @@ void bfs() {
 
 		for (auto itr = connected[node].begin(); itr != connected[node].end(); itr++) {
 			int n_node = itr->first;
-			int node_to_n_node = itr->second;
+			int value = itr->second;
 
 			if (!visited[n_node]) {
-				index[n_node] = ++idx;
-				dp[idx] = dp[idx - 1] + node_to_n_node;
 				visited[n_node] = true;
 				depth[n_node] = level + 1;
 				parent[n_node] = node;
+				sum[n_node] = sum[parent[n_node]] + connected[parent[n_node]][n_node];
 				q.emplace(std::make_pair(n_node, depth[n_node]));
 			}
 		}
@@ -52,8 +49,9 @@ void bfs() {
 }
 
 int LCA(int a, int b) {
-	int first_a = a;
-	int first_b = b;
+
+	int c_a = a;
+	int c_b = b;
 
 	if (depth[a] > depth[b]) std::swap(a, b);
 
@@ -66,7 +64,7 @@ int LCA(int a, int b) {
 		b = parent[b];
 	}
 
-	return dp[first_a] + dp[first_b] - (2 * dp[a]);
+	return (sum[c_a] - sum[a]) + (sum[c_b] - sum[a]);
 }
 
 int main() {
@@ -77,8 +75,7 @@ int main() {
 	visited.assign(N + 1, false);
 	depth.assign(N + 1, 0);
 	parent.assign(N + 1, 0);
-	index.assign(N + 1, 0);
-	dp.assign(N + 1, 0);
+	sum.assign(N + 1, 0);
 
 	for (int k = 0; k < N - 1; k++) {
 		scanf("%d %d %d", &a, &b, &c);
