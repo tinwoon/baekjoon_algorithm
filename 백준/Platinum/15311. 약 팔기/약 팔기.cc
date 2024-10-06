0228ac25a4c041c01dcc43df3bbb854701017161
Oh jiwon <tinwoon21@gmail.com>
2024-10-06 15:23:31 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,17 @@
+#pragma warning (disable : 4996)
+#include <iostream>
+#include <vector>
+
+int N;
+
+int main() {
+	scanf("%d", &N);
+	printf("2000\n");
+	for (int k = 0; k < 1000; k++) {
+		printf("1 ");
+	}
+	for (int k = 0; k < 1000; k++) {
+		printf("1000 ");
+	}
+
+}
